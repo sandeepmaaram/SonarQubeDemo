ab902ca14c8f731db7e4cb2b50e7da8f13fb780d
sandeepmaaram <sandeepmaaram@gmail.com>
2023-04-27 17:58:49 +0530

    SonarQube Integration

--- app/src/main/java/com/msr/sonarqubedemo/MainActivity.kt
+++ app/src/main/java/com/msr/sonarqubedemo/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,6 +28,18 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+       /* setContent {
+            SonarQubeDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Button("Android")
+                }
+            }
+        }*/
+
     }
 }
 
